2b1c8b0d9f65a9d985c033d03faf306a72a249ed
zhangguangyong <1243610991@qq.com>
2019-05-08 18:48:40 +0800

    demo

--- vue-study-elementui/element-starter/src/App.vue
+++ vue-study-elementui/element-starter/src/App.vue
@@ -1,24 +1,19 @@
 <template>
   <div id="app">
-    <el-container>
-      <el-header>Header</el-header>
-      <el-container>
-        <el-aside>
-          <navmenu></navmenu>
-        </el-aside>
-        <el-main>Main</el-main>
-      </el-container>
-    </el-container>
+    <!--<ContainerDemo/>-->
+    <ButtonDemo/>
   </div>
 </template>
 
 <script>
-  import NavMenu from '~/components/NavMenu'
+  import ContainerDemo from '~/components/ContainerDemo'
+  import ButtonDemo from '~/components/ButtonDemo'
 
   export default {
     name: 'app',
     components: {
-      'navmenu': NavMenu
+      'ContainerDemo': ContainerDemo,
+      'ButtonDemo': ButtonDemo
     }
   }
 </script>
@@ -27,5 +22,39 @@
   #app {
     font-family: Helvetica, sans-serif;
     text-align: center;
+  }
+
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
   }
 </style>
---
+++ vue-study-elementui/element-starter/src/components/ContainerDemo.vue
@@ -0,0 +1,55 @@
+<template>
+  <!--<el-container>-->
+  <!--<el-header>头部</el-header>-->
+  <!--<el-main>主体</el-main>-->
+  <!--</el-container>-->
+
+  <!--<el-container>-->
+  <!--<el-header>头部</el-header>-->
+  <!--<el-main>主体</el-main>-->
+  <!--<el-footer>底部</el-footer>-->
+  <!--</el-container>-->
+
+  <!--<el-container>-->
+  <!--<el-aside width="200px">侧边</el-aside>-->
+  <!--<el-main>主体</el-main>-->
+  <!--</el-container>-->
+
+  <!--<el-container>-->
+  <!--<el-header>头部</el-header>-->
+  <!--<el-container>-->
+  <!--<el-aside width="200px">侧边</el-aside>-->
+  <!--<el-main>主体</el-main>-->
+  <!--</el-container>-->
+  <!--</el-container>-->
+
+  <!--<el-container>-->
+  <!--<el-header>头部</el-header>-->
+  <!--<el-container>-->
+  <!--<el-aside width="200px">侧边</el-aside>-->
+  <!--<el-container>-->
+  <!--<el-main>主体</el-main>-->
+  <!--<el-footer>底部</el-footer>-->
+  <!--</el-container>-->
+  <!--</el-container>-->
+  <!--</el-container>-->
+
+  <el-container>
+    <el-aside width="200px">侧边</el-aside>
+    <el-container>
+      <el-header>头部</el-header>
+      <el-main>主体</el-main>
+      <el-footer>底部</el-footer>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+    export default {
+        name: "ContainerDemo"
+    }
+</script>
+
+<style scoped>
+
+</style>
