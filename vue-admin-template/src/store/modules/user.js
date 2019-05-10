f6155175a38587f3f390b949930c9cf31406fac1
zhangguangyong <1243610991@qq.com>
2019-05-10 19:45:11 +0800

    登录接口01

--- vue-admin-template/src/store/modules/user.js
+++ vue-admin-template/src/store/modules/user.js
@@ -27,8 +27,8 @@ const actions = {
     return new Promise((resolve, reject) => {
       login({ username: username.trim(), password: password }).then(response => {
         const { data } = response
-        commit('SET_TOKEN', data.token)
-        setToken(data.token)
+        commit('SET_TOKEN', data.accessToken)
+        setToken(data.accessToken)
         resolve()
       }).catch(error => {
         reject(error)
--- vue-admin-template/src/utils/request.js
+++ vue-admin-template/src/utils/request.js
@@ -14,12 +14,12 @@ const service = axios.create({
 service.interceptors.request.use(
   config => {
     // do something before request is sent
-
+debugger
     if (store.getters.token) {
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
+      config.headers['X-Access-Token'] = getToken()
     }
     return config
   },
@@ -46,7 +46,7 @@ service.interceptors.response.use(
     const res = response.data
 
     // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 0) {
+    if (res.code !== '0') {
       Message({
         message: res.message || 'error',
         type: 'error',
